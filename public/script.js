d1611579d03cfc8a72c4cd3fc3b0a2817f103b8f
Farshad Davari <DavariFarshad3@outlook.com>
2024-04-25 17:08:26 +0330

    Finish the project(withOut darkMode)

--- public/script.js
+++ public/script.js
@@ -1,7 +1,16 @@
 const hamburgerMenu = document.getElementById("hamburger-menu");
 const hamburgerItems = document.getElementById("hamburger-items");
+const hLinks = document.querySelectorAll("#hLink");
 
 hamburgerMenu.addEventListener("click", () => {
   hamburgerItems.classList.toggle("hidden");
   hamburgerMenu.classList.toggle("bg-white")
-})
+});
+
+hLinks.forEach(link => {
+  link.addEventListener("click", () => {
+    hamburgerItems.classList.toggle("hidden");
+    hamburgerMenu.classList.toggle("bg-white")
+  });
+});
+
